15cd425ecc7fe02f286e9790b006946b8d5b3cad
srSenhor <viniciusfeliciano14@gmail.com>
2023-09-07 01:08:39 -0300

    começo do passo a passo para algoritmos genéticos

--- alggen_min_func.cpp
+++ alggen_min_func.cpp
@@ -1,21 +1,91 @@
 #include <iostream>;
+#include <string>;
 #include <cmath>;
+#include <bitset> //Sugestão para visualizar binário
 #include <random>;
 
 using namespace std;
 
 //Função usada: raizquadrada(x^3 + 2*(y^4))
-inline double f(double x, double y){ return sqrt(pow(x, 3.0) + 2 * pow(y, 4.0)); }
+inline double f(int x, int y){ return sqrt(pow(x, 3) + 2 * pow(y, 4)); }
 
 
 //O algoritmo genético binário deve minimizar f(x) com x e y sendo valores no intervalo [0, 7]
 //Serão 3 bits para cada entrada.
 int main(){
-    //TODO: Definir qual função de geração de algoritmos aleatórios (provavelmente o mersennetwister)
-    //TODO: Definir o número de indivíduos da população
-    //TODO: Definir critérios para seleção de pares (provavelmente usando o método da roleta)     
-    //TODO: Definir taxa de mutação  
 
+    //Setando constantes
+    const int NUM_INDIVIDUOS = 8;
+    const float TAXA_MUTACAO = 0.05f;
+    const float PRECISAO = 1.0f;
+
+    //Setando seed para criação de números aleatórios
+    random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> dist(0, 7);
+
+    //Inicializando a população
+    int pop[NUM_INDIVIDUOS / 2][2]{};
+
+    for (int i = 0; i < NUM_INDIVIDUOS; i++)
+        for (int j = 0; j < 2; j++)
+            pop[i][j] = dist(mt);
+    
+    //Avaliando os indivíduos
+    double aval[NUM_INDIVIDUOS / 2]{};
+    double total_aval = 0.0;
+
+    for (int i = 0; i < NUM_INDIVIDUOS / 2; i++){
+        aval[i] = 1 + f(pop[i][0], pop[i][1]);  //Previnindo que a função de avaliação retorne valores negativos ou nulos, como no exemplo da sala
+        total_aval += aval[i];
+    }
+
+    //Repartindo os pedaços da roleta
+    double intervalos_roleta[NUM_INDIVIDUOS / 2]{};
+    for (int i = 0; i < NUM_INDIVIDUOS / 2; i++){
+        intervalos_roleta[i] = (aval[i] / total_aval) *  360;   //FIXME: inverter os valores já que se busca minimizar a função
+        intervalos_roleta[i] = round(intervalos_roleta[i]);     //FIXME: Em alguns casos, os pedaços somam 359. Buscar uma forma de contornar o problema.
+    }
+    
+    //TODO: A partir daqui, encapsule em um laço que realizará os passos seguintes
+    //TODO: Escrever o código do crossover dos pais selecionados aleatoriamente com base na roleta
+    //TODO: Escrever o código da mutação dos filhos. Crie um novo "gerador" randomico de 0 a 360
+    //TODO: Apague os membros da velha geração, mas não sei se são os pais selecionados ou o quê. Método ainda por definir.
+    //TODO: Escrever o código de reavaliação dos indivíduos. Considerar componentizar o código de avaliação já feito.
+
+
+
+    //Visualizando valores
+    cout << "Populacao de individuos" << endl;
+    for (int i = 0; i < NUM_INDIVIDUOS / 2; i++)
+        cout << "X: " << pop[i][0] << ", Y:" << pop[i][1] << endl;
+    cout << endl;
+
+    cout << "Avaliacao dos individuos sob f(x,y)" << endl;
+    for (int i = 0; i < NUM_INDIVIDUOS / 2; i++)
+        cout << i + 1 << ":" << aval[i] << endl;
+    cout << "Total: " << total_aval << endl << endl;
+
+    cout << "Parte da roleta (inicia em 0)" << endl;
+    for (int i = 0; i < NUM_INDIVIDUOS / 2; i++)
+        cout << i + 1 << ":" << intervalos_roleta[i] <<  " pedacos posteriores" << endl;
+    cout << "Total: " << total_aval << endl << endl;
+
+    
+    
+    
+
+    // Alguns testes para a visualização do binário
+    /*
+    int numero = 31;
+    bitset<32> binario(numero);
+
+    cout << "31: " << binario << endl;
+    */
+    /*
+    string binarioString = "11111";
+    cout << "Valor decimal:" << stoi(binarioString, nullptr, 2) << endl;
+    */
 
     return 0;
 }
